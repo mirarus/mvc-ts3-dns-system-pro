0bd2ef356a946e1d289f93df6ec9ca22206a2823
Ali Güçlü <aliguclutr@gmail.com>
2020-10-27 22:48:23 +0000

    Update database.sql

--- install/sql/database.sql
+++ install/sql/database.sql
@@ -113,7 +113,7 @@ CREATE TABLE `users`  (
   `mail` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `time` int(20) NOT NULL,
-  `login_ip` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `login_ip` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
